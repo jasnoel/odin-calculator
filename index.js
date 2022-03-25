46ad595c56d15daeafb222eb191bca90da3c7750
jasnoel <jason.noel.imp@gmail.com>
2022-03-25 17:02:54 +0100

    store and display operations

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ const operate = (operator, a, b) => {
             return `Error wrong operator: ${operator} isn't supported`;
     }
 }
+
+let operation = "";
+
+const display = document.querySelector('.display');
+
+const buttons = Array.from(document.querySelectorAll('button')).filter(btn => !(btn.classList.contains("AC") || btn.classList.contains("=")));
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+            operation += e.target.classList[0];
+            display.textContent = operation;
+        });
+});
