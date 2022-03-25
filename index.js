71d07182977f4544363ed7d63ae0fc51ffaa37de
jasnoel <jason.noel.imp@gmail.com>
2022-03-25 15:57:56 +0100

    create operate function

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 }
+
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case '+':
+          return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return `Error wrong operator: ${operator} isn't supported`;
+    }
+}
